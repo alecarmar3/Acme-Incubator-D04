7d4c7f96920242268cfb337e7c8ce1002726b3c3
Alejandro <alejandrosevilla200@gmail.com>
2020-08-20 17:48:41 +0200

    New indexes

--- src/main/java/acme/entities/Application.java
+++ src/main/java/acme/entities/Application.java
@@ -5,7 +5,9 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
@@ -23,6 +25,9 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "status, investor_id"), @Index(columnList = "investor_id")
+})
 public class Application extends DomainEntity {
 
 	// Serialization identifier -----------------------------------------------
